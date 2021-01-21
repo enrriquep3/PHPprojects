14a9e785f6718012fbcc557453d56f2e9f36800f
enrriquep3 <enrriquep3@gmail.com>
2021-01-21 11:12:37 -0500

    func

---
+++ func/arguments.php
@@ -0,0 +1,29 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+    <?php
+
+    $num1 = 5;
+    $num2 = 5;
+
+    //  parameters
+
+    function calculator($num1, $num2) {
+        echo $num1 * $num2;
+    }
+      
+    //arguments 
+    calculator($num1, $num2);
+
+
+
+
+    ?>
+</body>
+</html>

0f89e298bde07b36ef3ba799f12339be2b0f2674
enrriquep3 <enrriquep3@gmail.com>
2021-01-23 09:16:44 -0500

    uploadingtxt

---
+++ upload/uploads/fileout.php
@@ -0,0 +1,14 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+ <?php
+ $myFile = fopen("../uploads/file.txt", "w");
+
+ echo $myFile;
+ ?>   
+</body>
+</html>
